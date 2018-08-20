05e402b87f0590529e88c0b0c829ec1f36d06035
mWhitman189 <mwhitman189@gmail.com>
2018-08-20 22:49:16 +0900

    Set user login/logout buttons (unfinished)

--- views.py
+++ views.py
@@ -84,7 +84,7 @@ def inject_x_rate_headers(response):
 """
 
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+    newUser = User(username=login_session['username'], email=login_session['email'], picture=login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
